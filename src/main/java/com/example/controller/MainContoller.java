8d770cd4eac7b090856a47ddc9c3a90bb88a9413
Dmitry <43620456+JavaOneLove@users.noreply.github.com>
2019-12-02 23:57:22 +0300

    initial

---
+++ src/main/java/com/example/controller/MainContoller.java
@@ -0,0 +1,26 @@
+package com.example.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@RestController
+//@RequestMapping("/home")
+public class MainContoller {
+
+    static Logger LOGGER;
+
+    @GetMapping
+    public String test(){
+    return "test";
+    }
+    @PostMapping("/login")
+    public void ptest(){
+        LOGGER = Logger.getLogger(MainContoller.class.getName());
+        LOGGER.log(Level.INFO,"Метод отработал");
+    }
+}
